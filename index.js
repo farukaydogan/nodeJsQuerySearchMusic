30b8a55e48f276e89aac81e9d0da2a96b904736f
Faruk Aydogan <57232389+farukaydogan@users.noreply.github.com>
2023-03-13 17:55:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import { getPlaylistFromUrl } from '@vookav2/searchmusic'
+import { searchSong, searchArtist, searchAlbum, search } from '@vookav2/searchmusic'
+import express from 'express';
+
+const app = express();
+
+app.listen(4444, () =>{
+  console.log('Example app listening on port 3000!');
+});
+
+ app.get('/search', async (req, res) => {
+    const searchTerm = req.query.q;
+    const response = await searchSong(searchTerm)
+    res.send(response)
+    // do something with searchTerm, category, and tags
+  })
+
+
+
+  
